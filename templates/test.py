2f988b39206f7be22ade96646354b76d71636103
mietekdom <mieczyslaw.domansky@gmail.com>
2019-03-25 23:35:56 +0100

    changes

---
+++ templates/test.py
@@ -0,0 +1,58 @@
+<div class="container1">
+      <a class="container__link" href="#container-img1">
+        <a class="col"><img src="/media/{{trip.photo1}}"></a>
+      </a>
+      <div id="container-img1" class="container__overlay fadeIn">
+        <figure class="container__content container__figure">
+          <img src="/media/{{trip.photo1}}" alt="Demo image">
+          <div class="container__image" style="width: 800px; padding-top: 66.25%; background-image: url(img/img1.jpg);"></div>
+        </figure>
+        <a href="#container-untarget" class="container__close container__control">Close</a>
+        <a class="container__next container__control" href="#container-img2">Next</a>
+      </div>
+    </div>
+
+    <div class="container">
+      <a class="container__link" href="#container-img2">
+        <a class="col"><img src="/media/{{trip.photo2}}"></a>
+      </a>
+      <div id="container-img2" class="container__overlay fadeIn">
+        <figure class="container__content container__figure">
+          <img src="/media/{{trip.photo2}}" alt="Demo image">
+          <div class="perfundo__image" style="width: 800px; padding-top: 66.25%; background-image: url(img/img2.jpg);"></div>
+        </figure>
+        <a href="#perfundo-untarget" class="perfundo__close perfundo__control">Close</a>
+        <a class="container__next container__control" href="#container-img3">Next</a>
+        <a class="container__prev container__control" href="#container-img1">Prev</a>
+      </div>
+    </div>
+
+
+    <div class="container">
+      <a class="container__link" href="#container-img3">
+        <a class="col"><img src="/media/{{trip.photo3}}"></a>
+      </a>
+      <div id="container-img3" class="container__overlay fadeIn">
+        <figure class="container__content container__figure">
+          <img src="/media/{{trip.photo3}}" alt="Demo image">
+          <div class="container__image" style="width: 800px; padding-top: 66.25%; background-image: url(img/img3.jpg);"></div>
+        </figure>
+        <a href="#container-untarget" class="container__close container__control">Close</a>
+        <a class="container__prev container__control" href="#container-img2">Prev</a>
+      </div>
+    </div>
+
+
+    <div class="container">
+      <a class="container__link" href="#container-img4">
+        <a class="col"><img src="/media/{{trip.photo4}}"></a>
+      </a>
+      <div id="container-img4" class="container__overlay fadeIn">
+        <figure class="container__content container__figure">
+          <img src="/media/{{trip.photo4}}" alt="Demo image">
+          <div class="container__image" style="width: 800px; padding-top: 66.25%; background-image: url(img/img3.jpg);"></div>
+        </figure>
+        <a href="#container-untarget" class="container__close container__control">Close</a>
+        <a class="container__prev container__control" href="#container-img2">Prev</a>
+      </div>
+    </div>
